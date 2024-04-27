05caa6c0daa4b777a9c9260459c7845d687296f2
Ed MacDonald <edmacdonald555@gmail.com>
2024-04-27 00:37:33 -0400

    "fluent" or "clojure threading" VS nested method calls (ick; I dislike the latter)

--- src/main/java/com/consartist/spring/cache/BookAdvice.java
+++ src/main/java/com/consartist/spring/cache/BookAdvice.java
@@ -41,13 +41,18 @@ public class BookAdvice {
    public void tagCachedBook(ProceedingJoinPoint pjp, Object key, Object value) throws Throwable {
       // Flag cached records as having been cached. Don't cache host names.
       pjp.proceed(new Object[]{key,
-            tagBook(tagBook(value, "withCached", true, Boolean.class), "withHost", null, String.class)
+            BookTagger.of(value)
+                  .tag("withCached", true, Boolean.class)
+                  .tag("withHost", null, String.class)
+                  .get()
       });
    }
 
    @Around(value = "bookReturned() || cacheInterceptorInvoke()", argNames = "pjp")
    public Object tagReturnedBookWithHost(ProceedingJoinPoint pjp) throws Throwable {
-      return tagBook(pjp.proceed(), "withHost", InetAddress.getLocalHost().getHostName(), String.class);
+      return BookTagger.of(pjp.proceed())
+            .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
+            .get();
    }
 
    @Around(value = "booksReturned()", argNames = "pjp")
@@ -58,7 +63,9 @@ public class BookAdvice {
                      @Override
                      @SneakyThrows
                      public Object apply(Object o) {
-                        return tagBook(o, "withHost", InetAddress.getLocalHost().getHostName(), String.class);
+                        return BookTagger.of(o)
+                              .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
+                              .get();
                      }
                   }
             )
@@ -66,16 +73,33 @@ public class BookAdvice {
 
    }
 
-   @SneakyThrows
-   private Object tagBook(Object bookCandidate, @NonNull String tagMethodName, Object tagValue, @NonNull Class<?> tagValueClass) {
-      return bookCandidate == null ?
-            null :
-            // If you're using dev tools, value will have been loaded by the RefreshClassLoader.
-            // Book.class will be loaded by the AppClassLoader.
-            bookCandidate.getClass().getClassLoader().loadClass(Book.class.getName()).isInstance(bookCandidate) ?
-                  tagValue != null ?
-                        invokeMethod(bookCandidate, tagMethodName, tagValue) :
-                        invokeMethod(bookCandidate, tagMethodName, new Object[]{null}, new Class<?>[]{tagValueClass})
-                  : bookCandidate;
+   public static class BookTagger {
+      private Object maybeBook;
+
+      private BookTagger(Object maybeBook) {
+         this.maybeBook = maybeBook;
+      }
+
+      public static BookTagger of(Object maybeBook) {
+         return new BookTagger(maybeBook);
+      }
+
+      public Object get() {
+         return maybeBook;
+      }
+
+      @SneakyThrows
+      public BookTagger tag(@NonNull String tagMethodName, Object tagValue, @NonNull Class<?> tagValueClass) {
+         maybeBook = maybeBook == null ?
+               null :
+               // If you're using dev tools, value will have been loaded by the RefreshClassLoader.
+               // Book.class will be loaded by the AppClassLoader.
+               maybeBook.getClass().getClassLoader().loadClass(Book.class.getName()).isInstance(maybeBook) ?
+                     tagValue != null ?
+                           invokeMethod(maybeBook, tagMethodName, tagValue) :
+                           invokeMethod(maybeBook, tagMethodName, new Object[]{null}, new Class<?>[]{tagValueClass})
+                     : maybeBook;
+         return this;
+      }
    }
 }
