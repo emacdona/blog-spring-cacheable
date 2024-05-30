89aa4e460d9541d9b29ae57ac9932e45105f148e
Ed <edmacdonald555@gmail.com>
2024-05-30 05:49:47 +0000

    Added checkstyle to build

--- src/main/java/com/consartist/spring/cache/Application.java
+++ src/main/java/com/consartist/spring/cache/Application.java
@@ -6,6 +6,9 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Transient;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -27,27 +30,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
-import java.math.BigInteger;
-import java.util.Collection;
-
 @SpringBootApplication
 @EnableCaching
 @EnableLoadTimeWeaving
 @OpenAPIDefinition(
-        servers = {
-                @Server(url = "http://localhost:8080/", description = "Default Server URL."),
-                @Server(url = "http://host:8080/", description = "My strange dev setup URL.")
-        }
+    servers = {
+        @Server(url = "http://localhost:8080/", description = "Default Server URL."),
+        @Server(url = "http://host:8080/", description = "My strange dev setup URL.")
+    }
 )
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
 
 interface BookRepository extends JpaRepository<Book, Long>, ListCrudRepository<Book, Long> {
-    Book findByIsbn(String isbn);
+  Book findByIsbn(String isbn);
 }
 
 @Entity
@@ -55,113 +54,119 @@ interface BookRepository extends JpaRepository<Book, Long>, ListCrudRepository<B
 @NoArgsConstructor
 @AllArgsConstructor
 class Book implements Serializable {
-    @Id
-    @NonNull
-    private String isbn;
-    @With
-    private String title;
-    private String author;
-    @Transient
-    @With
-    private Boolean cached = false;
-    @Transient
-    @With
-    private String host = null;
+  @Id
+  @NonNull
+  private String isbn;
+  @With
+  private String title;
+  private String author;
+  @Transient
+  @With
+  private Boolean cached = false;
+  @Transient
+  @With
+  private String host = null;
 }
 
 @RestController
 @Slf4j
 class BookRestController {
-    private final BookRepository bookRepository;
+  private final BookRepository bookRepository;
 
-    @Autowired
-    public BookRestController(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
+  @Autowired
+  public BookRestController(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
 
-    @Operation(summary = "Clear 'books' cache.",
-            description = "Removes all cached books from the cache.")
-    @GetMapping("/books/clear")
-    @CacheEvict(cacheNames = "books", allEntries = true)
-    public void clearCache() {
-    }
+  @Operation(summary = "Clear 'books' cache.",
+      description = "Removes all cached books from the cache.")
+  @GetMapping("/books/clear")
+  @CacheEvict(cacheNames = "books", allEntries = true)
+  public void clearCache() {
+  }
 
-    @Operation(summary = "Retrieve all books.",
-            description = "Retrieves all books. Does not cache result(s).")
-    @GetMapping("/books")
-    public Collection<Book> books() {
-        return bookRepository.findAll();
-    }
+  @Operation(summary = "Retrieve all books.",
+      description = "Retrieves all books. Does not cache result(s).")
+  @GetMapping("/books")
+  public Collection<Book> books() {
+    return bookRepository.findAll();
+  }
 
-    @Operation(summary = "Retrieve a single book.",
-            description = "Retrieve a single book, identified by ISBN. Cache the result.")
-    @GetMapping("/books/{isbn}")
-    @Cacheable(cacheNames = "books")
-    public Book bookByIsbn(@PathVariable("isbn") String isbn) {
-        return bookRepository.findByIsbn(isbn);
-    }
+  @Operation(summary = "Retrieve a single book.",
+      description = "Retrieve a single book, identified by ISBN. Cache the result.")
+  @GetMapping("/books/{isbn}")
+  @Cacheable(cacheNames = "books")
+  public Book bookByIsbn(@PathVariable("isbn") String isbn) {
+    return bookRepository.findByIsbn(isbn);
+  }
 
-    @Operation(summary = "BAD: Update book title. Do not update cache.",
-            description = "BAD: Updates a single book's title, but does not add the updated book to the cache.")
-    @GetMapping("/books/{isbn}/badUpdateTitle/{title}")
-    public Book badUpdateTitle(@PathVariable("isbn") String isbn, @PathVariable("title") String title) {
-        return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
-    }
+  @SuppressWarnings("checkstyle:LineLength")
+  @Operation(summary = "BAD: Update book title. Do not update cache.",
+      description = "BAD: Updates a single book's title, but does not add the updated book to the cache.")
+  @GetMapping("/books/{isbn}/badUpdateTitle/{title}")
+  public Book badUpdateTitle(@PathVariable("isbn") String isbn,
+                             @PathVariable("title") String title) {
+    return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
+  }
 
-    @Operation(summary = "Update book title. Evict any cached copy of this book.",
-            description = "Updates a single book's title. Removes the book from the cache if present.")
-    @GetMapping("/books/{isbn}/betterUpdateTitle/{title}")
-    @CacheEvict(cacheNames = "books", key = "#isbn")
-    public Book betterUpdateTitle(@PathVariable("isbn") String isbn, @PathVariable("title") String title) {
-        return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
-    }
+  @Operation(summary = "Update book title. Evict any cached copy of this book.",
+      description = "Updates a single book's title. Removes the book from the cache if present.")
+  @GetMapping("/books/{isbn}/betterUpdateTitle/{title}")
+  @CacheEvict(cacheNames = "books", key = "#isbn")
+  public Book betterUpdateTitle(@PathVariable("isbn") String isbn,
+                                @PathVariable("title") String title) {
+    return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
+  }
 
-    @Operation(summary = "Update book title. Update cache with result",
-            description = "Updates a single book's title. Updates the cache with the result.")
-    @GetMapping("/books/{isbn}/bestUpdateTitle/{title}")
-    @CachePut(cacheNames = "books", key = "#isbn")
-    public Book bestUpdateTitle(@PathVariable("isbn") String isbn, @PathVariable("title") String title) {
-        return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
-    }
+  @Operation(summary = "Update book title. Update cache with result",
+      description = "Updates a single book's title. Updates the cache with the result.")
+  @GetMapping("/books/{isbn}/bestUpdateTitle/{title}")
+  @CachePut(cacheNames = "books", key = "#isbn")
+  public Book bestUpdateTitle(@PathVariable("isbn") String isbn,
+                              @PathVariable("title") String title) {
+    return bookRepository.save(bookRepository.findByIsbn(isbn).withTitle(title));
+  }
 }
 
 @RestController
 @Slf4j
 class FibonacciController {
-    // We need an injected reference so that Spring generated proxies run (because Spring generated
-    // proxies are how Caching -- and a slew of other Spring features -- are implemented.
-    @Autowired
-    @Lazy
-    FibonacciController self;
+  // We need an injected reference so that Spring generated proxies run (because Spring generated
+  // proxies are how Caching -- and a slew of other Spring features -- are implemented.
+  @Autowired
+  @Lazy
+  FibonacciController self;
 
-    @Operation(summary = "Find the nth Fibonacci number.",
-            description = "Finds this nth Fibonacci number without caching intermediate results. Slow!")
-    @GetMapping("/fibonacci/slow/{n}")
-    public BigInteger slowFibonacci(@PathVariable("n") BigInteger n) {
-        if (n.compareTo(BigInteger.ONE) < 0) {
-            return BigInteger.ZERO;
-        } else if (n.equals(BigInteger.ONE)) {
-            return BigInteger.ONE;
-        } else if (n.equals(BigInteger.TWO)) {
-            return BigInteger.ONE;
-        } else {
-            return self.slowFibonacci(n.subtract(BigInteger.ONE)).add(self.slowFibonacci(n.subtract(BigInteger.TWO)));
-        }
+  @Operation(summary = "Find the nth Fibonacci number.",
+      description = "Finds this nth Fibonacci number without caching intermediate results. Slow!")
+  @GetMapping("/fibonacci/slow/{n}")
+  public BigInteger slowFibonacci(@PathVariable("n") BigInteger n) {
+    if (n.compareTo(BigInteger.ONE) < 0) {
+      return BigInteger.ZERO;
+    } else if (n.equals(BigInteger.ONE)) {
+      return BigInteger.ONE;
+    } else if (n.equals(BigInteger.TWO)) {
+      return BigInteger.ONE;
+    } else {
+      return self.slowFibonacci(n.subtract(BigInteger.ONE))
+          .add(self.slowFibonacci(n.subtract(BigInteger.TWO)));
     }
+  }
 
-    @Operation(summary = "Find the nth Fibonacci number.",
-            description = "Finds this nth Fibonacci number -- and caches intermediate results. Fast!")
-    @GetMapping("/fibonacci/fast/{n}")
-    @Cacheable("fibonacci")
-    public BigInteger fastFibonacci(@PathVariable("n") BigInteger n) {
-        if (n.compareTo(BigInteger.ONE) < 0) {
-            return BigInteger.ZERO;
-        } else if (n.equals(BigInteger.ONE)) {
-            return BigInteger.ONE;
-        } else if (n.equals(BigInteger.TWO)) {
-            return BigInteger.ONE;
-        } else {
-            return self.fastFibonacci(n.subtract(BigInteger.ONE)).add(self.fastFibonacci(n.subtract(BigInteger.TWO)));
-        }
+  @Operation(summary = "Find the nth Fibonacci number.",
+      description = "Finds this nth Fibonacci number -- and caches intermediate results. Fast!")
+  @GetMapping("/fibonacci/fast/{n}")
+  @Cacheable("fibonacci")
+  public BigInteger fastFibonacci(@PathVariable("n") BigInteger n) {
+    if (n.compareTo(BigInteger.ONE) < 0) {
+      return BigInteger.ZERO;
+    } else if (n.equals(BigInteger.ONE)) {
+      return BigInteger.ONE;
+    } else if (n.equals(BigInteger.TWO)) {
+      return BigInteger.ONE;
+    } else {
+      return self.fastFibonacci(n.subtract(BigInteger.ONE))
+          .add(self.fastFibonacci(n.subtract(BigInteger.TWO)));
     }
+  }
 }
--- src/main/java/com/consartist/spring/cache/BookAdvice.java
+++ src/main/java/com/consartist/spring/cache/BookAdvice.java
@@ -1,5 +1,11 @@
 package com.consartist.spring.cache;
 
+import static org.apache.commons.lang3.reflect.MethodUtils.invokeMethod;
+
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -8,101 +14,99 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
-import java.net.InetAddress;
-import java.util.Collection;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
-import static org.apache.commons.lang3.reflect.MethodUtils.invokeMethod;
-
 @Aspect
 @Slf4j
 public class BookAdvice {
-   @Pointcut(value = "execution(void org.springframework.cache.Cache+.put(Object+, Object+))")
-   public void cachePut() {
-   }
+  @Pointcut(value = "execution(void org.springframework.cache.Cache+.put(Object+, Object+))")
+  public void cachePut() {
+  }
 
-   @Pointcut(value = "execution(Book com.consartist.spring.cache.BookRestController.*(..))")
-   public void bookReturned() {
-   }
+  @Pointcut(value = "execution(Book com.consartist.spring.cache.BookRestController.*(..))")
+  public void bookReturned() {
+  }
 
-   @Pointcut(value = "execution(java.util.Collection<Book> com.consartist.spring.cache.BookRestController.*(..))")
-   public void booksReturned() {
-   }
+  @SuppressWarnings("checkstyle:LineLength")
+  @Pointcut(value = "execution(java.util.Collection<Book> com.consartist.spring.cache.BookRestController.*(..))")
+  public void booksReturned() {
+  }
 
-   @Pointcut(value = "execution(Object org.springframework.cache.interceptor.CacheInterceptor.invoke(org.aopalliance.intercept.MethodInvocation))")
-   public void cacheInterceptorInvoke() {
-   }
+  @SuppressWarnings("checkstyle:LineLength")
+  @Pointcut(value = "execution(Object org.springframework.cache.interceptor.CacheInterceptor.invoke(org.aopalliance.intercept.MethodInvocation))")
+  public void cacheInterceptorInvoke() {
+  }
 
-   // this pointcut... and not the @Before one... necessitated this:
-   // --add-opens java.base/java.lang=ALL-UNNAMED.
-   //@Around(value = "execution(void org.springframework.cache.Cache+.put(Object+, Object+)) && args(key, value)", argNames = "pjp,key,value")
-   @Around(value = "cachePut() && args(key, value)", argNames = "pjp,key,value")
-   public void tagCachedBook(ProceedingJoinPoint pjp, Object key, Object value) throws Throwable {
-      // Flag cached records as having been cached. Don't cache host names.
-      pjp.proceed(new Object[]{key,
-            BookTagger.of(value)
-                  .tag("withCached", true, Boolean.class)
-                  .tag("withHost", null, String.class)
-                  .get()
-      });
-   }
+  // this pointcut... and not the @Before one... necessitated this:
+  // --add-opens java.base/java.lang=ALL-UNNAMED.
+  @Around(value = "cachePut() && args(key, value)", argNames = "pjp,key,value")
+  public void tagCachedBook(ProceedingJoinPoint pjp, Object key, Object value) throws Throwable {
+    // Flag cached records as having been cached. Don't cache host names.
+    pjp.proceed(new Object[] {key,
+        BookTagger.of(value)
+            .tag("withCached", true, Boolean.class)
+            .tag("withHost", null, String.class)
+            .get()
+    });
+  }
 
-   @Around(value = "bookReturned() || cacheInterceptorInvoke()", argNames = "pjp")
-   public Object tagReturnedBookWithHost(ProceedingJoinPoint pjp) throws Throwable {
-      return BookTagger.of(pjp.proceed())
-            .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
-            .get();
-   }
+  @Around(value = "bookReturned() || cacheInterceptorInvoke()", argNames = "pjp")
+  public Object tagReturnedBookWithHost(ProceedingJoinPoint pjp) throws Throwable {
+    return BookTagger.of(pjp.proceed())
+        .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
+        .get();
+  }
 
-   @Around(value = "booksReturned()", argNames = "pjp")
-   public Object tagReturnedBooksWithHost(ProceedingJoinPoint pjp) throws Throwable {
-      return ((Collection) pjp.proceed()).stream()
-            .map(
-                  new Function() {
-                     @Override
-                     @SneakyThrows
-                     public Object apply(Object o) {
-                        return BookTagger.of(o)
-                              .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
-                              .get();
-                     }
-                  }
-            )
-            .collect(Collectors.toList());
+  @Around(value = "booksReturned()", argNames = "pjp")
+  public Object tagReturnedBooksWithHost(ProceedingJoinPoint pjp) throws Throwable {
+    return ((Collection) pjp.proceed()).stream()
+        .map(
+            new Function() {
+              @Override
+              @SneakyThrows
+              public Object apply(Object o) {
+                return BookTagger.of(o)
+                    .tag("withHost", InetAddress.getLocalHost().getHostName(), String.class)
+                    .get();
+              }
+            }
+        )
+        .collect(Collectors.toList());
 
-   }
+  }
 
-   public static class BookTagger {
-      private Object maybeBook;
+  public static class BookTagger {
+    private Object maybeBook;
 
-      private BookTagger(Object maybeBook) {
-         this.maybeBook = maybeBook;
-      }
+    private BookTagger(Object maybeBook) {
+      this.maybeBook = maybeBook;
+    }
 
-      public static BookTagger of(Object maybeBook) {
-         return new BookTagger(maybeBook);
-      }
+    public static BookTagger of(Object maybeBook) {
+      return new BookTagger(maybeBook);
+    }
 
-      public Object get() {
-         return maybeBook;
-      }
+    public Object get() {
+      return maybeBook;
+    }
 
-      @SneakyThrows
-      public BookTagger tag(@NonNull String tagMethodName, Object tagValue, @NonNull Class<?> tagValueClass) {
-         maybeBook = maybeBook == null ?
-               null :
-               // https://stackoverflow.com/questions/1921238/getclass-getclassloader-is-null-why
-               (maybeBook.getClass().getClassLoader() != null &&
+    @SneakyThrows
+    public BookTagger tag(@NonNull String tagMethodName, Object tagValue,
+                          @NonNull Class<?> tagValueClass) {
+      maybeBook = maybeBook == null
+          ? null :
+          // https://stackoverflow.com/questions/1921238/getclass-getclassloader-is-null-why
+          (maybeBook.getClass().getClassLoader() != null
+              &&
 
-               // If you're using dev tools, value will have been loaded by the RefreshClassLoader.
-               // Book.class will be loaded by the AppClassLoader.
-               maybeBook.getClass().getClassLoader().loadClass(Book.class.getName()).isInstance(maybeBook)) ?
-                     tagValue != null ?
-                           invokeMethod(maybeBook, tagMethodName, tagValue) :
-                           invokeMethod(maybeBook, tagMethodName, new Object[]{null}, new Class<?>[]{tagValueClass})
-                     : maybeBook;
-         return this;
-      }
-   }
+              // If you're using dev tools, value will have been loaded by the RefreshClassLoader.
+              // Book.class will be loaded by the AppClassLoader.
+              maybeBook.getClass().getClassLoader().loadClass(Book.class.getName())
+                  .isInstance(maybeBook))
+              ? tagValue != null
+                  ? invokeMethod(maybeBook, tagMethodName, tagValue) :
+                  invokeMethod(maybeBook, tagMethodName, new Object[] {null},
+                      new Class<?>[] {tagValueClass})
+              : maybeBook;
+      return this;
+    }
+  }
 }
